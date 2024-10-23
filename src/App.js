ac48156a626b750d2d6616173a2ea031f87cb84b
Santiago Faé Poffo <santiagopoffo@hotmail.com>
2024-10-23 10:24:17 -0300

    docs: remove vendor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-import './assets/vendor/bootstrap/css/bootstrap.min.css'
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
-import './assets/vendor/boxicons/css/boxicons.min.css'
-import './assets/vendor/glightbox/css/glightbox.min.css'
 import Header from './Components/Header';
 import Hero from './Components/Hero';
 import Main from './Components/Main';
