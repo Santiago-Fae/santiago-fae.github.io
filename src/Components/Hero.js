56a090878829c3f241a25fe6e2134e1b083612ad
Santiago Faé Poffo <santiagopoffo@hotmail.com>
2025-04-16 19:57:56 -0700

    fix: open social media links in another page

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -37,22 +37,22 @@ export default function Hero() {
               </span>
             </p>
             <div class="social-links">
-              <a href="https://github.com/Santiago-Fae" aria-label='My github account'>
+               <a href="https://github.com/Santiago-Fae" target="_blank" rel="noopener noreferrer" aria-label="My github account">
                 <i class="bx bxl-github"></i>
               </a>
-              <a href="https://www.linkedin.com/in/santiago-fae-92322020a/" aria-label='My linkedin account'>
+              <a href="https://www.linkedin.com/in/santiago-fae-92322020a/" target="_blank" rel="noopener noreferrer" aria-label="My linkedin account">
                 <i class="bx bxl-linkedin"></i>
               </a>
-              <a href="mailto:santiagopoffo@hotmail.com" aria-label='My mail account'>
+              <a href="mailto:santiagopoffo@hotmail.com" target="_blank" rel="noopener noreferrer" aria-label="My mail account">
                 <i class="bx bx-envelope"></i>
               </a>
-              <a href="https://www.beecrowd.com.br/judge/pt/profile/573539" aria-label='My beecrowd profile'>
+              <a href="https://www.beecrowd.com.br/judge/pt/profile/573539" target="_blank" rel="noopener noreferrer" aria-label="My beecrowd profile">
                 <i class="bx bx-code-alt"></i>
               </a>
-              <a href="https://www.instagram.com/santi_fae/" aria-label='My instagram account'>
+              <a href="https://www.instagram.com/santi_fae/" target="_blank" rel="noopener noreferrer" aria-label="My instagram account">
                 <i class="bx bxl-instagram"></i>
               </a>
-              <a href="https://www.chess.com/member/santi_fae" aria-label='My chess account'>
+              <a href="https://www.chess.com/member/santi_fae" target="_blank" rel="noopener noreferrer" aria-label="My chess account">
                 <i class="bx bxs-chess"></i>
               </a>
             </div>
